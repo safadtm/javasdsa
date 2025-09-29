281459193a9e985b25cb6119e283078ee6a38cd8
safadtm <safadtm2004@gmail.com>
2025-09-29 21:14:35 +0530

    interfce

---
+++ interfaceprgm.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.Scanner;
+
+interface College{
+    void departmentName();
+    int numberOfStudents();
+}
+
+class ComputerScience implements College{
+
+    
+    public void departmentName() {
+    System.out.println("Department: Computer Science");
+    }
+
+    
+    public int numberOfStudents() {
+        return 150;
+    }
+
+}
+
+class Commerce implements College{
+
+    
+    public void departmentName() {
+    System.out.println("Department: Commerce");
+    }
+
+    
+    public int numberOfStudents() {
+        return 2000;
+    }
+    
+}
+
+public class interfaceprgm {
+    public static void main(String args[]){
+        College dept;
+
+        dept= new ComputerScience();
+        dept.departmentName();
+        System.out.println("Students :"+dept.numberOfStudents());
+
+        dept= new Commerce();
+        dept.departmentName();
+        System.out.println("Students :"+dept.numberOfStudents());
+    }
+}
